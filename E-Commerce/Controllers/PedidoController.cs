7e49b40a82d12892e73f57f4fd0fe1890beabe10
Pedro Dias <pedrooplx@gmail.com>
2020-07-25 00:32:58 -0300

    Criação de Interface

---
+++ E-Commerce/Controllers/PedidoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using E_Commerce.Models;
+
+namespace E_Commerce.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly ILogger<PedidoController> _logger;
+
+        public PedidoController(ILogger<PedidoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+        public IActionResult Carrinho()
+        {
+            return View();
+        }
+        public IActionResult Carrossel()
+        {
+            return View();
+        }
+        public IActionResult Resumo()
+        {
+            return View();
+        }
+    }
+}
--- E-Commerce/Startup.cs
+++ E-Commerce/Startup.cs
@@ -50,7 +50,7 @@ namespace E_Commerce
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
         }
     }
