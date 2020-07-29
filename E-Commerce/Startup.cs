6eaf2dda8ea1fbf9feb4557aa2010549d47e8d82
Pedro Dias <pedrooplx@gmail.com>
2020-07-29 00:05:20 -0300

    Database Migrate

--- E-Commerce/Startup.cs
+++ E-Commerce/Startup.cs
@@ -34,8 +34,8 @@ namespace E_Commerce
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline. Esse método consome os serviços.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -60,6 +60,9 @@ namespace E_Commerce
                     name: "default",
                     pattern: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
+
+            // Linha de código para garantir que o banco de dados tenha sido criado
+            serviceProvider.GetService<ApplicationContext>().Database.Migrate();
         }
     }
 }
