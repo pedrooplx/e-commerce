a9d8d50aa7ac44714fae4104fd4e859c1c3733c5
Pedro Dias <pedrooplx@gmail.com>
2020-08-08 23:56:12 -0300

    Criando ViewModel para tela de carrinho

--- E-Commerce/Controllers/PedidoController.cs
+++ E-Commerce/Controllers/PedidoController.cs
@@ -1,4 +1,5 @@
 ﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Models.ViewModels;
 using CasaDoCodigo.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -35,7 +36,9 @@ namespace CasaDoCodigo.Controllers
                 pedidoRepository.AddItem(codigo);
             }
 
-            return View(pedidoRepository.GetPedido().Itens);
+            List<ItemPedido> itens = pedidoRepository.GetPedido().Itens;
+            CarrinhoViewModel carrinhoViewModel = new CarrinhoViewModel(itens);
+            return base.View(carrinhoViewModel);
         }
 
         public IActionResult Cadastro()
