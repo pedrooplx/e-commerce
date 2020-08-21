6201bce6e584ec6782467e1693cdf7652613139c
Pedro Dias <pedrooplx@gmail.com>
2020-08-20 22:14:08 -0300

    Adicionando novos atributos de usuários em AppIdentityUser - AppIdentityContext - e atualizando banco de dados com Migration

--- CasaDoCodigo/Migrations/AppIdentity/20200821011028_UsuarioCustomizado.Designer.cs
+++ CasaDoCodigo/Migrations/AppIdentity/20200821011028_UsuarioCustomizado.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CasaDoCodigo.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CasaDoCodigo.Migrations.AppIdentity
 {
     [DbContext(typeof(AppIdentityContext))]
-    partial class AppIdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20200821011028_UsuarioCustomizado")]
+    partial class UsuarioCustomizado
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
