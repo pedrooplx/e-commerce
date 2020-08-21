b43b086d34c85bc826a9db30bfaeece1a36306ac
Pedro Dias <pedrooplx@gmail.com>
2020-08-20 22:30:24 -0300

    Obtendo e salvando dados do usuário com a classe UserManager

--- CasaDoCodigo/Controllers/PedidoController.cs
+++ CasaDoCodigo/Controllers/PedidoController.cs
@@ -62,6 +62,14 @@ namespace CasaDoCodigo.Controllers
             var usuario = await userManager.GetUserAsync(this.User);
 
             pedido.Cadastro.Email = usuario.Email;
+            pedido.Cadastro.Telefone = usuario.Telefone;
+            pedido.Cadastro.Nome = usuario.Nome;
+            pedido.Cadastro.Endereco = usuario.Endereco;
+            pedido.Cadastro.Complemento = usuario.Complemento;
+            pedido.Cadastro.Bairro = usuario.Bairro;
+            pedido.Cadastro.Municipio = usuario.Municipio;
+            pedido.Cadastro.UF = usuario.UF;
+            pedido.Cadastro.CEP = usuario.CEP;
 
             return View(pedido.Cadastro);
         }
@@ -73,6 +81,21 @@ namespace CasaDoCodigo.Controllers
         {
             if (ModelState.IsValid)
             {
+
+                var usuario = await userManager.GetUserAsync(this.User);
+
+                usuario.Email = cadastro.Email;
+                usuario.Telefone = cadastro.Telefone;
+                usuario.Nome = cadastro.Nome;
+                usuario.Endereco = cadastro.Endereco;
+                usuario.Complemento = cadastro.Complemento;
+                usuario.Bairro = cadastro.Bairro;
+                usuario.Municipio = cadastro.Municipio;
+                usuario.UF = cadastro.UF;
+                usuario.CEP = cadastro.CEP;
+
+                await userManager.UpdateAsync(usuario);
+
                 return View(await pedidoRepository.UpdateCadastroAsync(cadastro));
             }
             return RedirectToAction("Cadastro");
