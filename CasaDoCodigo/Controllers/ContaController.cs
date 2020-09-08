c08e4288412b2b71d7717079b4d98c38cbb3bc70
Pedro Dias <pedrooplx@gmail.com>
2020-09-07 23:50:09 -0300

    criando ContaController e actions de login e logout

---
+++ CasaDoCodigo/Controllers/ContaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class ContaController : Controller
+    {
+        [Authorize]
+        public async Task<IActionResult> Login()
+        {
+            return Redirect(Url.Content("~/")); //"~/" para ir para a HomePage
+        }
+        
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync("Cookies"); //Serve para atualizar os cookies de sessão (autenticação local)
+            await HttpContext.SignOutAsync("OpenIdConnect"); //Serve para desconectar o usuário no IdentityServer
+            return Redirect(Url.Content("~/"));
+        }
+    }
+}
