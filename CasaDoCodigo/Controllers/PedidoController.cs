f35558f2504df061849591e3f4428087c8ae5bcb
Pedro Dias <pedrooplx@gmail.com>
2020-09-08 00:10:26 -0300

    adaptando a teela de cadastro para o Identity Server

--- CasaDoCodigo/Controllers/PedidoController.cs
+++ CasaDoCodigo/Controllers/PedidoController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace CasaDoCodigo.Controllers
@@ -64,17 +65,7 @@ namespace CasaDoCodigo.Controllers
                 return RedirectToAction("Carrossel");
             }
 
-            var usuario = await userManager.GetUserAsync(this.User);
-
-            pedido.Cadastro.Email = usuario.Email;
-            pedido.Cadastro.Telefone = usuario.Telefone;
-            pedido.Cadastro.Nome = usuario.Nome;
-            pedido.Cadastro.Endereco = usuario.Endereco;
-            pedido.Cadastro.Complemento = usuario.Complemento;
-            pedido.Cadastro.Bairro = usuario.Bairro;
-            pedido.Cadastro.Municipio = usuario.Municipio;
-            pedido.Cadastro.UF = usuario.UF;
-            pedido.Cadastro.CEP = usuario.CEP;
+            pedido.Cadastro.Nome = User.FindFirst("name")?.Value;
 
             return View(pedido.Cadastro);
         }
@@ -86,20 +77,6 @@ namespace CasaDoCodigo.Controllers
         {
             if (ModelState.IsValid)
             {
-                var usuario = await userManager.GetUserAsync(this.User);
-
-                usuario.Email       = cadastro.Email;
-                usuario.Telefone    = cadastro.Telefone;
-                usuario.Nome        = cadastro.Nome;
-                usuario.Endereco    = cadastro.Endereco;
-                usuario.Complemento = cadastro.Complemento;
-                usuario.Bairro      = cadastro.Bairro;
-                usuario.Municipio   = cadastro.Municipio;
-                usuario.UF          = cadastro.UF;
-                usuario.CEP         = cadastro.CEP;
-
-                await userManager.UpdateAsync(usuario);
-
                 return View(await pedidoRepository.UpdateCadastroAsync(cadastro));
             }
             return RedirectToAction("Cadastro");
