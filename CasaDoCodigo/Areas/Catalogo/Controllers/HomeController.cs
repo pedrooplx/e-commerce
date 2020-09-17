430f60fa152619af626ee7a3d9f83300c851102d
Pedro Dias <pedrooplx@gmail.com>
2020-09-16 21:59:02 -0300

    Controller de Área e Roteamento de Área - Catalogo

---
+++ CasaDoCodigo/Areas/Catalogo/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CasaDoCodigo.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigo.Areas.Catalogo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProdutoRepository produtoRepository;
+
+        public HomeController(IProdutoRepository produtoRepository)
+        {
+            this.produtoRepository = produtoRepository;
+        }
+
+        public async Task<IActionResult> Index(string pesquisa)
+        {
+            return View(await produtoRepository.GetProdutosAsync(pesquisa));
+        }
+    }
+}
